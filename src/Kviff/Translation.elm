e072aac667eaac92783af62c31e05f6e982a21b4
pravdomil <pravdomil.toman@gmail.com>
2021-08-25 20:56:48 +0200

    Translation.time

--- src/Kviff/Translation.elm
+++ src/Kviff/Translation.elm
@@ -1,6 +1,7 @@
 module Kviff.Translation exposing (..)
 
 import Kviff.Api as Api
+import Time
 
 
 title : String
@@ -30,6 +31,61 @@ type_ a =
 
         Api.Restaurant ->
             "Restaurant"
+
+
+time : Time.Posix -> String
+time a =
+    let
+        zone : Time.Zone
+        zone =
+            Time.customZone 0 []
+
+        monthToInt : Time.Month -> Int
+        monthToInt b =
+            case b of
+                Time.Jan ->
+                    1
+
+                Time.Feb ->
+                    2
+
+                Time.Mar ->
+                    3
+
+                Time.Apr ->
+                    4
+
+                Time.May ->
+                    5
+
+                Time.Jun ->
+                    6
+
+                Time.Jul ->
+                    7
+
+                Time.Aug ->
+                    8
+
+                Time.Sep ->
+                    9
+
+                Time.Oct ->
+                    10
+
+                Time.Nov ->
+                    11
+
+                Time.Dec ->
+                    12
+    in
+    String.fromInt (Time.toDay zone a)
+        ++ ". "
+        ++ String.fromInt (monthToInt (Time.toMonth zone a))
+        ++ ". "
+        ++ String.padLeft 2 '0' (String.fromInt (Time.toHour zone a))
+        ++ ":"
+        ++ String.padLeft 2 '0' (String.fromInt (Time.toMinute zone a))
 
 
 duration : Int -> String
