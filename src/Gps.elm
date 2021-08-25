de53ef036eccd7ed24e0d8dab39b27b600d45490
pravdomil <pravdomil.toman@gmail.com>
2021-08-25 23:47:34 +0200

    Gps module

---
+++ src/Gps.elm
@@ -0,0 +1,34 @@
+module Gps exposing (..)
+
+import Json.Decode as D
+import Parser as P exposing ((|.), (|=))
+import Parser.DeadEnd as DeadEnd
+
+
+type alias Gps =
+    { lat : Float
+    , lon : Float
+    }
+
+
+decode : D.Decoder Gps
+decode =
+    let
+        parser : P.Parser Gps
+        parser =
+            P.succeed Gps
+                |= P.float
+                |. P.symbol ","
+                |= P.float
+                |. P.end
+    in
+    D.string
+        |> D.andThen
+            (\v ->
+                case P.run parser v of
+                    Err b ->
+                        D.fail (DeadEnd.toString b)
+
+                    Ok b ->
+                        D.succeed b
+            )
