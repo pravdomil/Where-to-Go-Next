07991208ce7706d17eef47430f354761e01839bf
pravdomil <pravdomil.toman@gmail.com>
2021-08-29 22:45:48 +0200

    Utils.Html module

---
+++ src/Utils/Html.elm
@@ -0,0 +1,27 @@
+module Utils.Html exposing (..)
+
+import Html.Parser
+import Parser
+
+
+stripTags : String -> Result (List Parser.DeadEnd) String
+stripTags a =
+    let
+        nodesToString : List Html.Parser.Node -> String
+        nodesToString b =
+            List.map nodeToString b |> String.join ""
+
+        nodeToString : Html.Parser.Node -> String
+        nodeToString b =
+            case b of
+                Html.Parser.Text c ->
+                    c
+
+                Html.Parser.Element _ _ c ->
+                    nodesToString c
+
+                Html.Parser.Comment _ ->
+                    ""
+    in
+    Html.Parser.run a
+        |> Result.map nodesToString
