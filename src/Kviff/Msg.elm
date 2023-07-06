9ef26dc7c1654dd390d9165380f9e3d7430236d2
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-07-06 22:40:58 +0200

    next

--- src/Kviff/Msg.elm
+++ src/Kviff/Msg.elm
@@ -9,5 +9,7 @@ import Time
 type Msg
     = LocaleChangeRequested Kviff.Api.Locale
     | TimeReceived Time.Posix
+      --
     | DataReceived (Result Http.Error Kviff.Api.Data)
+      --
     | ViewportSet (Result Browser.Dom.Error ())
