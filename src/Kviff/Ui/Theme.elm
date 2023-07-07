a801485fcbd315f3c5aedbab75f4a04a803df6e2
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-07-07 11:55:35 +0200

    next

---
+++ src/Kviff/Ui/Theme.elm
@@ -0,0 +1,11 @@
+module Kviff.Ui.Theme exposing (..)
+
+import Element.PravdomilUi.Theme.Basic
+
+
+theme =
+    Element.PravdomilUi.Theme.Basic.theme style
+
+
+style =
+    Element.PravdomilUi.Theme.Basic.style
