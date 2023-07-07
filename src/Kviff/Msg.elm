c9f8907a22a8ad65179b0d737c95a8497bbcc625
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-07-07 11:42:12 +0200

    next

--- src/Kviff/Msg.elm
+++ src/Kviff/Msg.elm
@@ -3,11 +3,12 @@ module Kviff.Msg exposing (..)
 import Browser.Dom
 import Http
 import Kviff.Data
+import Kviff.Locale
 import Time
 
 
 type Msg
-    = LocaleRequested Kviff.Data.Locale
+    = LocaleRequested Kviff.Locale.Locale
     | TimeReceived Time.Posix
       --
     | DataReceived (Result Http.Error Kviff.Data.Data)
