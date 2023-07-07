1144975c6343b3cb7550a20e8770b98e15cc7a6e
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-07-07 14:33:42 +0200

    next

--- src/Kviff/Gps.elm
+++ src/Kviff/Gps.elm
@@ -3,12 +3,23 @@ module Kviff.Gps exposing (..)
 import Json.Decode
 import Parser
 import Parser.DeadEnd
+import Url
 
 
 type alias Gps =
     { lat : Float
     , lon : Float
     }
+
+
+mapyCzLink : Gps -> String
+mapyCzLink a =
+    "https://mapy.cz/?z=16&y="
+        ++ Url.percentEncode (String.fromFloat a.lat)
+        ++ "&x="
+        ++ Url.percentEncode (String.fromFloat a.lon)
+        ++ "&q="
+        ++ Url.percentEncode (String.fromFloat a.lat ++ " " ++ String.fromFloat a.lon)
 
 
 decoder : Json.Decode.Decoder Gps
