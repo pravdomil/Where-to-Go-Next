703992cd0e485fbd30c8c2d5405db08503356147
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-07-07 20:09:30 +0200

    next

--- src/Kviff/Model/Utils.elm
+++ src/Kviff/Model/Utils.elm
@@ -11,12 +11,13 @@ relevantEvents : Kviff.Model.Model -> List ( Id.Id Kviff.Data.Event, Kviff.Data.
 relevantEvents model =
     case model.data of
         Ok data ->
-            List.filter
-                (\( _, x ) ->
-                    Time.posixToMillis (Kviff.Data.eventTime x) + Kviff.Data.eventDuration x > Time.posixToMillis model.time
-                )
-                (Dict.Any.toList data.events)
+            List.filter (eventIsRelevant model) (Dict.Any.toList data.events)
                 |> List.sortBy (\( _, x ) -> Time.posixToMillis (Kviff.Data.eventTime x))
 
         Err _ ->
             []
+
+
+eventIsRelevant : Kviff.Model.Model -> ( Id.Id Kviff.Data.Event, Kviff.Data.Event ) -> Bool
+eventIsRelevant model ( _, a ) =
+    Time.posixToMillis (Kviff.Data.eventTime a) + Kviff.Data.eventDuration a > Time.posixToMillis model.time
