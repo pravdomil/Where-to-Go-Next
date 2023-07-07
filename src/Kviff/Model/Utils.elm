c102fb6e0bbf0799b31ece53b6781ead6ad47b7f
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-07-07 16:05:50 +0200

    next

--- src/Kviff/Model/Utils.elm
+++ src/Kviff/Model/Utils.elm
@@ -9,16 +9,11 @@ import Time
 
 relevantEvents : Kviff.Model.Model -> List ( Id.Id Kviff.Data.Event, Kviff.Data.Event )
 relevantEvents model =
-    let
-        hour : Int
-        hour =
-            60 * 60 * 1000
-    in
     case model.data of
         Ok data ->
             List.filter
                 (\( _, x ) ->
-                    Time.posixToMillis (Kviff.Data.eventTime x) > (Time.posixToMillis model.time - hour)
+                    Time.posixToMillis (Kviff.Data.eventTime x) + Kviff.Data.eventDuration x > Time.posixToMillis model.time
                 )
                 (Dict.Any.toList data.events)
                 |> List.sortBy (\( _, x ) -> Time.posixToMillis (Kviff.Data.eventTime x))
