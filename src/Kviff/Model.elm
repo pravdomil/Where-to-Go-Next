551b8ee44227ccd9ad64c80cb986610d5438f4c1
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-07-07 11:42:01 +0200

    next

--- src/Kviff/Model.elm
+++ src/Kviff/Model.elm
@@ -2,11 +2,12 @@ module Kviff.Model exposing (..)
 
 import Http
 import Kviff.Data
+import Kviff.Locale
 import Time
 
 
 type alias Model =
-    { locale : Kviff.Data.Locale
+    { locale : Kviff.Locale.Locale
     , time : Maybe Time.Posix
     , data : Result Error Kviff.Data.Data
     }
