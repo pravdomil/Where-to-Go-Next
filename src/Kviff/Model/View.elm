dc9ffac792a3b26535757ca3368c0e3898ecb882
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-07-07 20:02:15 +0200

    next

--- src/Kviff/Model/View.elm
+++ src/Kviff/Model/View.elm
@@ -107,7 +107,12 @@ viewEvents : Kviff.Model.Model -> Kviff.Data.Data -> Element Kviff.Msg.Msg
 viewEvents model a =
     column [ width fill, spacing 20 ]
         (Dict.Any.toList a.events
-            |> List.sortBy (\( _, x ) -> Time.posixToMillis (Kviff.Data.eventTime x))
+            |> List.sortBy
+                (\( _, x ) ->
+                    ( Time.posixToMillis (Kviff.Data.eventTime x)
+                    , Kviff.Data.eventDuration x
+                    )
+                )
             |> List.foldl
                 (\( id, x ) ( acc, time ) ->
                     let
