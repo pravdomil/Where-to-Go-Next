bb43bb50eac1a50d32624bbf2ee71751cc13bb10
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-07-25 22:25:41 +0200

    next

---
+++ src/Festival/Utils/Time.elm
@@ -0,0 +1,43 @@
+module Festival.Utils.Time exposing (..)
+
+import Time
+
+
+monthToInt : Time.Month -> number
+monthToInt a =
+    case a of
+        Time.Jan ->
+            1
+
+        Time.Feb ->
+            2
+
+        Time.Mar ->
+            3
+
+        Time.Apr ->
+            4
+
+        Time.May ->
+            5
+
+        Time.Jun ->
+            6
+
+        Time.Jul ->
+            7
+
+        Time.Aug ->
+            8
+
+        Time.Sep ->
+            9
+
+        Time.Oct ->
+            10
+
+        Time.Nov ->
+            11
+
+        Time.Dec ->
+            12
