07afc5d2679c2de211abd78071c62b36a5224410
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-10-25 15:57:24 +0200

    next

--- src/Festival/Utils/Theme.elm
+++ src/Festival/Utils/Theme.elm
@@ -1,43 +1,59 @@
 module Festival.Utils.Theme exposing (..)
 
-import Element.PravdomilUi exposing (..)
-import Element.PravdomilUi.Theme
-import Element.PravdomilUi.Theme.Basic
+import Element exposing (..)
 
 
-theme : Element.PravdomilUi.Theme.Theme msg
-theme =
-    Element.PravdomilUi.Theme.Basic.theme style
-        |> (\x ->
-                { x
-                    | page = \x2 -> x.page (bgColor style.back100 :: x2)
-                }
-           )
+back100 =
+    rgb 0 0 0
 
 
-style : Element.PravdomilUi.Theme.Style
-style =
-    let
-        a =
-            Element.PravdomilUi.Theme.Basic.style
-    in
-    { a
-        | back100 = rgb 0 0 0
-        , back90 = rgb 0.12 0.14 0.15
-        , back80 = rgb 0.19 0.22 0.24
-        , back70 = rgb 0.28 0.31 0.34
-        , back60 = rgb 0.41 0.45 0.48
-        , back50 = rgb 0.54 0.58 0.61
+back90 =
+    rgb 0.12 0.14 0.15
 
-        --
-        , fore100 = rgb 1 1 1
-        , fore90 = rgb 0.96 0.97 0.97
-        , fore80 = rgb 0.9 0.92 0.93
-        , fore70 = rgb 0.86 0.88 0.89
-        , fore60 = rgb 0.8 0.82 0.84
-        , fore50 = rgb 0.67 0.7 0.73
 
-        --
-        , primaryBack = rgb 0.2471 0.5961 1
-        , primaryFore = rgb 1 1 1
-    }
+back80 =
+    rgb 0.19 0.22 0.24
+
+
+back70 =
+    rgb 0.28 0.31 0.34
+
+
+back60 =
+    rgb 0.41 0.45 0.48
+
+
+back50 =
+    rgb 0.54 0.58 0.61
+
+
+fore100 =
+    rgb 1 1 1
+
+
+fore90 =
+    rgb 0.96 0.97 0.97
+
+
+fore80 =
+    rgb 0.9 0.92 0.93
+
+
+fore70 =
+    rgb 0.86 0.88 0.89
+
+
+fore60 =
+    rgb 0.8 0.82 0.84
+
+
+fore50 =
+    rgb 0.67 0.7 0.73
+
+
+primaryBack =
+    rgb 0.2471 0.5961 1
+
+
+primaryFore =
+    rgb 1 1 1
